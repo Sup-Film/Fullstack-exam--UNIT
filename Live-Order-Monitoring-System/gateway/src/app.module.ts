66b353a793167d042acf37d06c274e4610ae4cd0
SupakornDPU <sup.dongkew@gmail.com>
2025-07-24 01:42:54 +0700

    feat(gateway): add WebSocket adapter

--- Live-Order-Monitoring-System/gateway/src/app.module.ts
+++ Live-Order-Monitoring-System/gateway/src/app.module.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
